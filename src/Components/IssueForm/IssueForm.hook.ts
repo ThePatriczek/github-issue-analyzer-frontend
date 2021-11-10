620d327395d86496323d1ec1962776d55a2e6c58
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-11-10 21:29:44 +0100

    chore(deps): upgade

--- src/Components/IssueForm/IssueForm.hook.ts
+++ src/Components/IssueForm/IssueForm.hook.ts
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+import {yupResolver} from '@hookform/resolvers/yup/dist/yup';
 import { Estimation, IssueFormFields } from "./IssueForm.types";
 import { issueFormSchema, IssueFormSchemaFields } from "./IssueForm.schema";
 import { useQuery } from "react-query";
